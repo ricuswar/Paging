229e2d7edad37ae41493d48d07d3949cf595c91e
Ricus Warmenhoven <ricusw@gmail.com>
2019-04-10 22:45:44 +0200

    added alot

--- Paging_Program/Paging_Program/Form1.Designer.cs
+++ Paging_Program/Paging_Program/Form1.Designer.cs
@@ -35,11 +35,17 @@
             this.AccessSequanceButton = new System.Windows.Forms.Button();
             this.AccessSequanceListBox = new System.Windows.Forms.ListBox();
             this.ASLLB = new System.Windows.Forms.Label();
-            this.ASDUD = new System.Windows.Forms.DomainUpDown();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.ASUD = new System.Windows.Forms.NumericUpDown();
             this.SetSizeButton = new System.Windows.Forms.Button();
             this.SetValButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numHDD = new System.Windows.Forms.NumericUpDown();
+            this.lsbPages = new System.Windows.Forms.ListBox();
+            this.btnGetPages = new System.Windows.Forms.Button();
+            this.ASCB = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ASUD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHDD)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -90,6 +96,7 @@
             this.AccessSequanceButton.TabIndex = 4;
             this.AccessSequanceButton.Text = "Create random access Squance";
             this.AccessSequanceButton.UseVisualStyleBackColor = true;
+            this.AccessSequanceButton.Click += new System.EventHandler(this.AccessSequanceButton_Click);
             // 
             // AccessSequanceListBox
             // 
@@ -109,30 +116,23 @@
             this.ASLLB.TabIndex = 7;
             this.ASLLB.Text = "Access Sequance Length";
             // 
-            // ASDUD
+            // ASUD
             // 
-            this.ASDUD.Location = new System.Drawing.Point(124, 190);
-            this.ASDUD.Name = "ASDUD";
-            this.ASDUD.Size = new System.Drawing.Size(80, 22);
-            this.ASDUD.TabIndex = 8;
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(39, 119);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.ASUD.Location = new System.Drawing.Point(39, 119);
+            this.ASUD.Maximum = new decimal(new int[] {
             20,
             0,
             0,
             0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.ASUD.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(165, 22);
-            this.numericUpDown1.TabIndex = 9;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.ASUD.Name = "ASUD";
+            this.ASUD.Size = new System.Drawing.Size(165, 22);
+            this.ASUD.TabIndex = 9;
+            this.ASUD.Value = new decimal(new int[] {
             1,
             0,
             0,
@@ -146,6 +146,7 @@
             this.SetSizeButton.TabIndex = 10;
             this.SetSizeButton.Text = "Set Size";
             this.SetSizeButton.UseVisualStyleBackColor = true;
+            this.SetSizeButton.Click += new System.EventHandler(this.SetSizeButton_Click);
             // 
             // SetValButton
             // 
@@ -155,16 +156,90 @@
             this.SetValButton.TabIndex = 11;
             this.SetValButton.Text = "Set Val";
             this.SetValButton.UseVisualStyleBackColor = true;
+            this.SetValButton.Click += new System.EventHandler(this.SetValButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(644, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Number of pages in harddrrive ";
+            // 
+            // numHDD
+            // 
+            this.numHDD.Location = new System.Drawing.Point(659, 119);
+            this.numHDD.Maximum = new decimal(new int[] {
+            26,
+            0,
+            0,
+            0});
+            this.numHDD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numHDD.Name = "numHDD";
+            this.numHDD.Size = new System.Drawing.Size(165, 22);
+            this.numHDD.TabIndex = 13;
+            this.numHDD.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lsbPages
+            // 
+            this.lsbPages.FormattingEnabled = true;
+            this.lsbPages.ItemHeight = 16;
+            this.lsbPages.Location = new System.Drawing.Point(702, 147);
+            this.lsbPages.Name = "lsbPages";
+            this.lsbPages.Size = new System.Drawing.Size(79, 276);
+            this.lsbPages.TabIndex = 14;
+            // 
+            // btnGetPages
+            // 
+            this.btnGetPages.Location = new System.Drawing.Point(659, 427);
+            this.btnGetPages.Name = "btnGetPages";
+            this.btnGetPages.Size = new System.Drawing.Size(165, 59);
+            this.btnGetPages.TabIndex = 15;
+            this.btnGetPages.Text = "Get pages from HDD";
+            this.btnGetPages.UseVisualStyleBackColor = true;
+            this.btnGetPages.Click += new System.EventHandler(this.btnGetPages_Click);
+            // 
+            // ASCB
+            // 
+            this.ASCB.FormattingEnabled = true;
+            this.ASCB.Location = new System.Drawing.Point(124, 191);
+            this.ASCB.Name = "ASCB";
+            this.ASCB.Size = new System.Drawing.Size(81, 24);
+            this.ASCB.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 67);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 498);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ASCB);
+            this.Controls.Add(this.btnGetPages);
+            this.Controls.Add(this.lsbPages);
+            this.Controls.Add(this.numHDD);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.SetValButton);
             this.Controls.Add(this.SetSizeButton);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.ASDUD);
+            this.Controls.Add(this.ASUD);
             this.Controls.Add(this.ASLLB);
             this.Controls.Add(this.AccessSequanceListBox);
             this.Controls.Add(this.AccessSequanceButton);
@@ -175,7 +250,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ASUD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHDD)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -190,10 +266,15 @@
         private System.Windows.Forms.Button AccessSequanceButton;
         private System.Windows.Forms.ListBox AccessSequanceListBox;
         private System.Windows.Forms.Label ASLLB;
-        private System.Windows.Forms.DomainUpDown ASDUD;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown ASUD;
         private System.Windows.Forms.Button SetSizeButton;
         private System.Windows.Forms.Button SetValButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numHDD;
+        private System.Windows.Forms.ListBox lsbPages;
+        private System.Windows.Forms.Button btnGetPages;
+        private System.Windows.Forms.ComboBox ASCB;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Paging_Program/Paging_Program/Form1.cs
+++ Paging_Program/Paging_Program/Form1.cs
@@ -15,9 +15,60 @@ namespace Paging_Program
         public Form1()
         {
             InitializeComponent();
+            
         }
 
+        public int setsize = 0;
+
         private void Form1_Load(object sender, EventArgs e)
+        {
+            AccessSequanceListBox.Items.Clear();
+        }
+
+        private void btnGetPages_Click(object sender, EventArgs e)
+        {
+            lsbPages.Items.Clear();
+            int amount = int.Parse(numHDD.Text);
+
+            for (int i = 1; i <= amount; i++)
+            {
+                lsbPages.Items.Add(Convert.ToChar(i + 64));
+                ASCB.Items.Add(Convert.ToChar(i + 64));
+                
+            }
+        }
+
+        private void SetSizeButton_Click(object sender, EventArgs e)
+        {
+            setsize = int.Parse(ASUD.Text);
+        }
+
+        private void SetValButton_Click(object sender, EventArgs e)
+        {
+            if (setsize > 0)
+            {
+                setsize = setsize - 1;
+                AccessSequanceListBox.Items.Add(ASCB.Text);
+            }
+            else
+            {
+                MessageBox.Show("sorry no space left");
+            }
+            
+
+        }
+
+        private void AccessSequanceButton_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < setsize; i++)
+            {
+                int temp = rnd.Next(setsize);
+                AccessSequanceListBox.Items.Add(Convert.ToChar(temp + 64));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
             AccessSequanceListBox.Items.Clear();
         }
